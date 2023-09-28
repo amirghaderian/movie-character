14bad4a7c59d260e86dcfb2dc887358d2e828b76
asd <asd.it@gmail.com>
2023-09-28 17:03:12 +0330

    Dynamic navbar header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { allCharacters } from "../data/data";
 import "./App.css";
 import CharacterDetail from "./components/CharacterDetail";
@@ -5,7 +6,8 @@ import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="app"><Navbar />
-  <div className="main"><CharacterList allCharacters={allCharacters}/><CharacterDetail/></div></div>;
+  const [characters, setCharacter] = useState(allCharacters)
+  return <div className="app"><Navbar searchResult={characters}/>
+  <div className="main"><CharacterList allCharacters={characters}/><CharacterDetail/></div></div>;
 };
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import {HeartIcon as Heart} from "@heroicons/react/24/outline"
-const Navbar = () => {
+const Navbar = ({searchResult}) => {
   return (
   <nav className="navbar">
     <div className="navbar__logo">LOGO 😍</div>
 
     <input type="text" className="text-field" placeholder="search .." />
-    <div className="navbar__result">Found X characters </div>
+    <div className="navbar__result">Found {searchResult.length} characters </div>
     <button className="heart"><Heart className="icon"/> <span className="badge">4</span></button>
   </nav>
     )
