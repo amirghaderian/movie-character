67aa1c6fa6ffc4a1d08608ccc21d26cbe1595167
asd <asd.it@gmail.com>
2023-10-02 19:00:13 +0330

    Clean up data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -10,44 +10,87 @@ const App = () => {
   const [characters, setCharacter] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState("");
-  const [selectedId, setSelectedId] = useState(null)
-  const [favourites, setFavourites] = useState([])
+  const [selectedId, setSelectedId] = useState(null);
+  const [favourites, setFavourites] = useState([]);
+  const [count, setCount] = useState(0);
   useEffect(() => {
+    const interval = setInterval(() => setCount((c) => c + 1), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
     const fetchData = async () => {
       try {
         setIsLoading(true);
         const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character/?name=${query}`
+          `https://rickandmortyapi.com/api/character/?name=${query}`,
+          { signal }
         );
         setCharacter(data.results.slice(0, 10));
         setIsLoading(false);
       } catch (err) {
-        toast.error(err.response.data.error);
-        setCharacter([]);
-        setIsLoading(false);
+        if (!axios.isCancel()) {
+          toast.error(err.response.data.error);
+
+          console.log(err.name);
+          setCharacter([]);
+          setIsLoading(false);
+        }
       }
     };
-    if (query.length <= 2) {
-      setCharacter([]);
-      return;
-    }
-    fetchData();
-  }, [query]);
-const handleSelectedCaracter = (id) =>{
-  setSelectedId(preveId => preveId===id ? null :id)
-}
-const handleAddFavourite = (character) =>{
-  setFavourites([...favourites,character])
-}
-const isAddToFavourit = favourites.map(fav=>fav.id).includes(selectedId)
-return (
-    <div className="app">
-      <Toaster />
-      <Navbar searchResult={characters} query={query} setQuery={setQuery} numOfFavorites={favourites.length} />
-      <div className="main">
-        <CharacterList allCharacters={characters} isLoading={isLoading}  onSelectCharacter={handleSelectedCaracter} selectedId={selectedId}/>
 
-        <CharacterDetail selectedId={selectedId} setSelectedId={setSelectedId} handleSelectedCaracter={handleSelectedCaracter} onAddFavourite={handleAddFavourite} isAddToFavourit={isAddToFavourit}/>
+    // if (query.length <= 2) {
+    //   setCharacter([]);
+    //   return;
+    // }
+    fetchData();
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+  const handleSelectedCaracter = (id) => {
+    setSelectedId((preveId) => (preveId === id ? null : id));
+  };
+  const handleAddFavourite = (character) => {
+    setFavourites([...favourites, character]);
+  };
+  const isAddToFavourit = favourites.map((fav) => fav.id).includes(selectedId);
+  return (
+    <div className="app">
+      <div style={{ color: "#fff" }}>
+        {count}{" "}
+        <button
+          style={{ color: "#fff" }}
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          add 1
+        </button>
+      </div>
+      <Toaster />
+      <Navbar
+        searchResult={characters}
+        query={query}
+        setQuery={setQuery}
+        numOfFavorites={favourites.length}
+      />
+      <div className="main">
+        <CharacterList
+          allCharacters={characters}
+          isLoading={isLoading}
+          onSelectCharacter={handleSelectedCaracter}
+          selectedId={selectedId}
+        />
+
+        <CharacterDetail
+          selectedId={selectedId}
+          setSelectedId={setSelectedId}
+          handleSelectedCaracter={handleSelectedCaracter}
+          onAddFavourite={handleAddFavourite}
+          isAddToFavourit={isAddToFavourit}
+        />
       </div>
     </div>
   );
